39a0f159752b037f31d7b2f2b9975bd76c9897ac
William Dias <smdwilliam89@gmail.com>
2024-09-25 12:28:43 -0300

    feat(Cards): Criando o componente Cards.

---
+++ src/components/Bootstrap/Cards.jsx
@@ -0,0 +1,50 @@
+// Import the required modules.
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const CardContainer = ({
+    image,
+    title,
+    cardInfo,
+    githubLink,
+    depoloyedLink,
+}) => {
+    return (
+        <>
+            {['Dark'].map((variant) => (
+                <Card
+                    bg={variant.toLowerCase()}
+                    key={variant}
+                    text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+                    style={{ width: '18rem' }}
+                >
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>{cardInfo}</Card.Text>
+                    </Card.Body>
+                    <Card.Body>
+                        <Button
+                            variant="light"
+                            style={{ marginRight: '10px' }}
+                            href={githubLink}
+                            target="_blank"
+                        >
+                            GitHub
+                        </Button>
+
+                        <Button
+                            variant="info"
+                            href={depoloyedLink}
+                            target="_blank"
+                        >
+                            Deployed
+                        </Button>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    );
+};
+
+export default CardContainer;
